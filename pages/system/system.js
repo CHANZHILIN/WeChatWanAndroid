de79cd67a90c8ce67e8adf06a085cc833695ac5f
CHANZHILIN <1181785848@qq.com>
2021-08-12 23:51:57 +0800

    体系 flex布局

--- pages/system/system.js
+++ pages/system/system.js
@@ -5,15 +5,19 @@ Page({
      * 页面的初始数据
      */
     data: {
-        toView: "",
         systemHeaderList: [],
         currentSelected: 0,
-        dataArray: []
+        // 体系数据
+        dataArray: [],
+        //导航数据
+        navDataArray: [],
+        //导航-左侧栏选择位置
+        navCurrentSelected:0
     },
 
     /**
- * 公众号作者列表数据
- */
+     * 公众号作者列表数据
+     */
     getOfficicalTitleListData: function () {
         let that = this
         let suffixUrl = '/wxarticle/chapters/json'
@@ -38,54 +42,75 @@ Page({
                 let id = that.data.systemHeaderList[that.data.currentSelected].id
                 that.getDataList(id)
             },
-            (err) => { })
+            (err) => {})
     },
     /**
-    * 获取数据
-    * @param {类型，1为体系，2为导航} type
-    */
+     * 获取数据
+     * @param {类型，1为体系，2为导航} type
+     */
     getDataList: function (type) {
         let that = this
         let suffixUrl = type == 'system' ? '/tree/json' : '/navi/json'
         app.wxRequest("GET", suffixUrl, null,
             (res) => {
                 let list = new Array()
-                res.forEach((item, index, array) => {
-                    var subName = ""
-                    item.children.forEach((childItem, childIndex, childArray) => {
-                        subName =subName+ (childIndex+1) +":"+ childItem.name + "\t"
-                    }
-                    ),
+                if (type == 'system') { //体系数据
+                    res.forEach((item, index, array) => {
+                        var subName = ""
+                        item.children.forEach((childItem, childIndex, childArray) => {
+                                subName = subName + (childIndex + 1) + ":" + childItem.name + "\t"
+                            }),
+                            //添加数据
+                            list.push({
+                                title: item.name,
+                                content: subName
+                            })
+                    })
+
+                    that.setData({
+                        ["dataArray[0]"]: list
+                    })
+                } else { //导航数据
+                    res.forEach((item, index, array) => {
                         //添加数据
                         list.push({
+                            isCheck: index == 0 ? true : false,
+                            id: item.cid,
                             title: item.name,
-                            content: subName
+                            articles: item.articles
                         })
-                })
-
-                that.setData({
-                    ["dataArray[0]"]: list
-                })
+                    })
+                    that.setData({
+                        navDataArray: list
+                    })
+                }
                 wx.stopPullDownRefresh()
             },
-            (err) => { })
+            (err) => {})
     },
     //头部导航点击选中
     onSystemHeaderClick: function (event) {
-        var toughtIndex = event.currentTarget.dataset.index
-        var oldSelected = this.data.currentSelected
+        let toughtIndex = event.currentTarget.dataset.index
+        let oldSelected = this.data.currentSelected
 
-
-        let moveToMiddleIndex = toughtIndex - 2 < 0 ? 0 : toughtIndex - 2
         this.setData({
             ["systemHeaderList[" + oldSelected + "].isCheck"]: false,
             ["systemHeaderList[" + toughtIndex + "].isCheck"]: true,
-            currentSelected: toughtIndex,
-            toView: this.data.systemHeaderList[moveToMiddleIndex].viewId
+            currentSelected: toughtIndex
         })
         let id = this.data.systemHeaderList[toughtIndex].id
         this.getDataList(id)
 
+    },
+    //导航页-左侧导航选择
+    onNavClick:function(event){
+        let toughtIndex = event.currentTarget.dataset.index
+        let oldSelected =  this.data.navCurrentSelected
+        this.setData({
+            ["navDataArray[" + oldSelected + "].isCheck"]: false,
+            ["navDataArray[" + toughtIndex + "].isCheck"]: true,
+            navCurrentSelected: toughtIndex
+        })
     },
     /**
      * 生命周期函数--监听页面加载
