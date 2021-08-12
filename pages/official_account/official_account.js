21b42418de9ef1f009195fd8d55a9d8f5b343241
CHANZHILIN <1181785848@qq.com>
2021-08-12 17:38:16 +0800

    体系

--- pages/official_account/official_account.js
+++ pages/official_account/official_account.js
@@ -3,106 +3,116 @@
 const app = getApp()
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        officialList: [],
-        currentSelected: 0,
-        dataArray:[],
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: "",
+    officialList: [],
+    currentSelected: 0,
+    dataArray: [],
     //当前页码
     pageNum: 0,
     //总页数
-    totalPageNume:0
-    },
+    totalPageNume: 0
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        this.getOfficicalTitleListData()
-    },
-    //点击
-    onOfficialClick: function (event) {
-        var toughtIndex = event.currentTarget.dataset.index
-        var oldSelected = this.data.currentSelected
-        this.setData({
-            ["officialList[" + oldSelected + "].isCheck"]: false,
-            ["officialList[" + toughtIndex + "].isCheck"]: true,
-            currentSelected: toughtIndex
-        })
-        let id = this.data.officialList[this.data.currentSelected].id
-        this.getData(true,id)
-    },
-    /**
-     * 公众号作者列表数据
-     */
-    getOfficicalTitleListData: function () {
-        let that = this
-        let suffixUrl = '/wxarticle/chapters/json'
-        app.wxRequest("GET", suffixUrl, null,
-            (res) => {
-                let list = new Array()
-                res.forEach((item, index, array) => {
-                    list.push({
-                        id: item.id,
-                        courseId: item.courseId,
-                        name: item.name,
-                        isCheck: index == 0 ? true : false
-                    })
-                })
-              
-                that.setData({
-                    officialList: list
-                })
-                let id = that.data.officialList[that.data.currentSelected].id
-                that.getData(true,id)
-              
-            },
-            (err) => { })
-    },
-
-      /**
-   * 获取公众号数据
-   * @param {是否刷新} isRefresh 
-   * @param {公众号id} id
+  /**
+   * 生命周期函数--监听页面加载
    */
-  getData: function (isRefresh,id) {
+  onLoad: function (options) {
+    this.getOfficicalTitleListData()
+  },
+  //头部导航点击选中
+  onOfficialClick: function (event) {
+    var toughtIndex = event.currentTarget.dataset.index
+    var oldSelected = this.data.currentSelected
+
+
+    let moveToMiddleIndex = toughtIndex - 2 < 0 ? 0 : toughtIndex - 2
+    this.setData({
+      ["officialList[" + oldSelected + "].isCheck"]: false,
+      ["officialList[" + toughtIndex + "].isCheck"]: true,
+      currentSelected: toughtIndex,
+      toView: this.data.officialList[moveToMiddleIndex].viewId
+    })
+    let id = this.data.officialList[toughtIndex].id
+    this.getDataList(true, id)
+
+  },
+  /**
+   * 公众号作者列表数据
+   */
+  getOfficicalTitleListData: function () {
+    let that = this
+    let suffixUrl = '/wxarticle/chapters/json'
+    app.wxRequest("GET", suffixUrl, null,
+      (res) => {
+        let list = new Array()
+        res.forEach((item, index, array) => {
+          list.push({
+            id: item.id,
+            viewId: 'official_' + item.id,
+            courseId: item.courseId,
+            name: item.name,
+            isCheck: index == 0 ? true : false
+          })
+        })
+
+        that.setData({
+          officialList: list
+        })
+
+        let id = that.data.officialList[that.data.currentSelected].id
+        that.getDataList(true, id)
+        that.setData({
+          toView: that.data.officialList[that.data.currentSelected].viewId
+        })
+      },
+      (err) => { })
+  },
+
+  /**
+* 获取公众号数据
+* @param {是否刷新} isRefresh 
+* @param {公众号id} id
+*/
+  getDataList: function (isRefresh, id) {
     let that = this
     let currentPageNum = isRefresh ? 0 : that.data.pageNum + 1
     //大于最大页码时候 返回
-    if(currentPageNum > that.data.totalPageNume) return
-    let suffixUrl = '/wxarticle/list/'+id+'/' +currentPageNum + '/json'
+    if (currentPageNum > that.data.totalPageNume) return
+    let suffixUrl = '/wxarticle/list/' + id + '/' + currentPageNum + '/json'
     app.wxRequest("GET", suffixUrl, null,
-        (res) => {
-            let list = new Array()
-            res.datas.forEach((item, index, array) => {
-              //添加数据
-              list.push({
-                id:item.id,
-                author: item.author != ""? item.author:item.shareUser,
-                isSetTop: index == 0 || index == 1 ? true : false,
-                isNew:item.fresh,
-                time: item.niceDate,
-                content: item.title,
-                original: item.superChapterName + "/" + item.chapterName,
-                isFocus: item.collect,
-                link: item.link
-              })
-            })
-            if (isRefresh) {
-              that.setData({
-                dataArray: [],
-                totalPageNume:res.pageCount
-              })
-            }
-              that.setData({
-                ["dataArray["+currentPageNum+"]"]: list,
-                pageNum:currentPageNum
-              })
-              wx.stopPullDownRefresh()
-        },
-        (err) => { })
+      (res) => {
+        let list = new Array()
+        res.datas.forEach((item, index, array) => {
+          //添加数据
+          list.push({
+            id: item.id,
+            author: item.author != "" ? item.author : item.shareUser,
+            isSetTop: index == 0 || index == 1 ? true : false,
+            isNew: item.fresh,
+            time: item.niceDate,
+            content: item.title,
+            original: item.superChapterName + "/" + item.chapterName,
+            isFocus: item.collect,
+            link: item.link
+          })
+        })
+        if (isRefresh) {
+          that.setData({
+            dataArray: [],
+            totalPageNume: res.pageCount
+          })
+        }
+        that.setData({
+          ["dataArray[" + currentPageNum + "]"]: list,
+          pageNum: currentPageNum
+        })
+        wx.stopPullDownRefresh()
+      },
+      (err) => { })
   },
   /**
    * 点击时事件
@@ -113,7 +123,7 @@ Page({
     if (this.data.dataArray[arrayIndex][toughIndex].link != null) {
       wx.navigateTo({
         // url: this.data.dataArray[arrayIndex][toughIndex].link,
-        url: '../web_view/webView?artUrl='+this.data.dataArray[arrayIndex][toughIndex].link
+        url: '../web_view/webView?artUrl=' + this.data.dataArray[arrayIndex][toughIndex].link
       })
     }
   },
@@ -124,46 +134,46 @@ Page({
     var arrayIndex = event.currentTarget.dataset.array
     var toughIndex = event.currentTarget.dataset.index
     this.setData({
-      ["dataArray["+arrayIndex+"]["+toughIndex+"].isFocus"]:!this.data.dataArray[arrayIndex][toughIndex].isFocus
+      ["dataArray[" + arrayIndex + "][" + toughIndex + "].isFocus"]: !this.data.dataArray[arrayIndex][toughIndex].isFocus
       // ['articleList[' + toughIndex + '].isFocus']: !this.data.articleList[toughIndex].isFocus
     })
 
   },
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
-    },
+  },
 
-   
+
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
     let id = this.data.officialList[this.data.currentSelected].id
-    this.getData(true,id)
+    this.getDataList(true, id)
     setTimeout(() => {
       wx.stopPullDownRefresh()
     }, 5000)
@@ -174,13 +184,13 @@ Page({
    */
   onReachBottom: function () {
     let id = this.data.officialList[this.data.currentSelected].id
-    this.getData(false,id)
+    this.getDataList(false, id)
   },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-    }
+  }
 })
--- pages/system/system.js
+++ pages/system/system.js
@@ -1,18 +1,97 @@
 // pages/system/system.js
+let app = getApp();
 Page({
-
     /**
      * 页面的初始数据
      */
     data: {
-
+        toView: "",
+        systemHeaderList: [],
+        currentSelected: 0,
+        dataArray: []
     },
 
+    /**
+ * 公众号作者列表数据
+ */
+    getOfficicalTitleListData: function () {
+        let that = this
+        let suffixUrl = '/wxarticle/chapters/json'
+        app.wxRequest("GET", suffixUrl, null,
+            (res) => {
+                let list = new Array()
+                list.push({
+                    id: 'system',
+                    name: "体系",
+                    isCheck: true
+                })
+                list.push({
+                    id: 'nav',
+                    name: "导航",
+                    isCheck: false
+                })
+
+                that.setData({
+                    systemHeaderList: list
+                })
+
+                let id = that.data.systemHeaderList[that.data.currentSelected].id
+                that.getDataList(id)
+            },
+            (err) => { })
+    },
+    /**
+    * 获取数据
+    * @param {类型，1为体系，2为导航} type
+    */
+    getDataList: function (type) {
+        let that = this
+        let suffixUrl = type == 'system' ? '/tree/json' : '/navi/json'
+        app.wxRequest("GET", suffixUrl, null,
+            (res) => {
+                let list = new Array()
+                res.forEach((item, index, array) => {
+                    var subName = ""
+                    item.children.forEach((childItem, childIndex, childArray) => {
+                        subName =subName+ (childIndex+1) +":"+ childItem.name + "\t"
+                    }
+                    ),
+                        //添加数据
+                        list.push({
+                            title: item.name,
+                            content: subName
+                        })
+                })
+
+                that.setData({
+                    ["dataArray[0]"]: list
+                })
+                wx.stopPullDownRefresh()
+            },
+            (err) => { })
+    },
+    //头部导航点击选中
+    onSystemHeaderClick: function (event) {
+        var toughtIndex = event.currentTarget.dataset.index
+        var oldSelected = this.data.currentSelected
+
+
+        let moveToMiddleIndex = toughtIndex - 2 < 0 ? 0 : toughtIndex - 2
+        this.setData({
+            ["systemHeaderList[" + oldSelected + "].isCheck"]: false,
+            ["systemHeaderList[" + toughtIndex + "].isCheck"]: true,
+            currentSelected: toughtIndex,
+            toView: this.data.systemHeaderList[moveToMiddleIndex].viewId
+        })
+        let id = this.data.systemHeaderList[toughtIndex].id
+        this.getDataList(id)
+
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.getOfficicalTitleListData();
     },
 
     /**
@@ -47,7 +126,8 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
-
+        let id = this.data.systemHeaderList[this.data.currentSelected].id
+        this.getDataList(id)
     },
 
     /**
